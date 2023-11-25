72d07257e7673cefe115f939552d5f158802459c
Hossain-sakib <mskib567@gmail.com>
2023-11-25 13:25:38 +0600

    added navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Zen Zephyr | Home</title>
+            </Helmet>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
